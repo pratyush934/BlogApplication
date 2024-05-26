f6c63dbab67d1902e1ed5d311625c68c39dc75ae
Pratyush <oggycandle786@gmail.com>
2024-05-26 13:04:28 +0530

    Adding

--- blog-application-backend/src/main/java/com/blogapplication/blog_application_backend/BlogApplicationBackendApplication.java
+++ blog-application-backend/src/main/java/com/blogapplication/blog_application_backend/BlogApplicationBackendApplication.java
@@ -2,12 +2,17 @@ package com.blogapplication.blog_application_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BlogApplicationBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplicationBackendApplication.class, args);
 	}
+
+
 
 }
