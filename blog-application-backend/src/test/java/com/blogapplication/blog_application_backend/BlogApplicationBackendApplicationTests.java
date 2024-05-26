50f63520d92781234508f86ce7195cbe83237f17
Pratyush <oggycandle786@gmail.com>
2024-05-26 12:54:47 +0530

    Adding backend files

---
+++ blog-application-backend/src/test/java/com/blogapplication/blog_application_backend/BlogApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogapplication.blog_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
